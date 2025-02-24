ce4ef13b1ba202c1287d93934601feb3e2218e4f
harris <marabutharris_bsit@plmun.edu.ph>
2025-02-24 23:19:47 +0800

    Submitting OBJPROG Week 05 - Laboratory # 16

---
+++ MethodOverloading.java
@@ -0,0 +1,22 @@
+public class MethodOverloading {
+    
+    public static void printValue(int value) {
+        System.out.println("Integer value: " + value);
+    }
+
+
+    public static void printValue(double value) {
+        System.out.println("Double value: " + value);
+    }
+
+    
+    public static void printValue(String value) {
+        System.out.println("String value: " + value);
+    }
+
+    public static void main(String[] args) {
+        printValue(10);       
+        printValue(3.14);     
+        printValue("Hello!"); 
+    }
+}
